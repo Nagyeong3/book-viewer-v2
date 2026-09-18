4e0f7c87298ea213175097cf04b0053dfb97689b
Nagyeong3 <109929166+Nagyeong3@users.noreply.github.com>
2026-09-18 11:31:40 +0900

    refactor: remove scroll pulse loading guard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import ProductToolbar from "../components/ProductToolbar";
-import ScrollLoadGuard from "../components/ScrollLoadGuard";
 import WorkspaceV3 from "../components/WorkspaceV3";
 
 export default function HomePage() {
   return (
     <>
       <WorkspaceV3 />
-      <ScrollLoadGuard />
       <ProductToolbar />
     </>
   );
